308b96884ef6130f5edd8473af3be3650974c6e3
Shani Aharon <rivena14798@gmail.com>
2023-09-15 11:01:28 +0300

    change

--- server.js
+++ server.js
@@ -178,6 +178,7 @@ app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 app.listen(port, () => {
+    if (process.env.SECRET1) console.log('process.env.SECRET1', process.env.SECRET1)
     console.log(`App listening on port ${port}!`)
 });
 
